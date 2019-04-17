97e8312769f0f3ed8ed59655bcd9036f61246c4d
nelsonflorez11 <nelsonflorez11@gmail.com>
2019-04-17 00:15:07 -0500

    Taller FLISOL V3

--- semillas/cpllaves.sh
+++ semillas/cpllaves.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-for i in 192.168.56.101 192.168.56.102;do
-     su - "root" -c "sshpass -ppasswordroot ssh-copy-id $i";
+for i in flisol1 flisol2;do
+     su - "root" -c "sshpass -ptux ssh-copy-id $i";
 done
-for i in 192.168.56.101 192.168.56.102;do
-     su - "prueba" -c "sshpass -ppasswordprueba ssh-copy-id $i";
+for i in flisol1 flisol2;do
+     su - "flisol" -c "sshpass -pflisol2019 ssh-copy-id $i";
 done
+
